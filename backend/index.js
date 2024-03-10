382cfc75cebedf96ca52a08862a7d7ff4bd6361d
dishaap <pandurangidisha@gmail.com>
2024-03-09 23:03:36 -0800

    adds backend

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,30 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const port = 3005;
+const axios = require("axios");
+const port = process.env.PORT || 3000;
 const app = express();
 const readline = require("readline");
 const fs = require("fs");
+const fetchArticles = require('./fetchArticles');
+const generateQuestion = require('./generateQuestion');
 
 const articles = require('./articles.json');
 const articlesData = Object.values(articles);
 
-// const questions = require('./questions.json');
-// const questionsData = Object.values(questions);
+const questions = require('./questions.json');
+const questionsData = Object.values(questions);
 
-// cohere requirements
-const { CohereClient } = require("cohere-ai");
-const cohere = new CohereClient({
-  token: "xlg4vfmNFDyLwEaPtrTqFdI3rPLvL1Um7D53jMKN",
-});
+// console.log(questionsData);
 
 // example on local port
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.post('/fetch_articles', fetchArticles);
+
+app.post('/generate_question', generateQuestion);
 
 // listening on port 3000
 app.listen(port, () => {
@@ -29,47 +32,54 @@ app.listen(port, () => {
 });
 
 // web search for articles related to given prompt (varied prompts)
-(async () => {
-  const chatStream = await cohere.chatStream({
-    chatHistory: [
-    ],
-    message: "Fetch articles on marine pollution in the Pacific North-West",
-    // perform web search before answering the question. You can also use your own custom connector.
-    connectors: [{ id: "web-search" }],
-    stream: true
-  });
+// (async () => {
+//   const chatStream = await cohere.chatStream({
+//     chatHistory: [
+//     ],
+//     message: "Fetch articles on marine pollution in the Pacific North-West",
+//     // perform web search before answering the question. You can also use your own custom connector.
+//     connectors: [{ id: "web-search" }],
+//     stream: true
+//   });
 
-  for await (const message of chatStream) {
-    // console.log(message);
-    if (message.eventType = "search-results" && 'documents' in message) {
-      console.log(message.documents);
-      fs.writeFile("articles.json", JSON.stringify(message.documents), function (err) {
-        if (err) throw err;
-        console.log('complete');
-      }
-      );
-    }
-    // if(message.eventType="citation-generation") {
-    //   console.log(message);
-    // }
-  }
-})();
+//   for await (const message of chatStream) {
+//     // console.log(message);
+//     if (message.eventType = "search-results" && 'documents' in message) {
+//       console.log(message.documents);
+//       fs.writeFile("articles.json", JSON.stringify(message.documents), function (err) {
+//         if (err) throw err;
+//         console.log('complete');
+//       }
+//       );
+//     }
+//     // if(message.eventType="citation-generation") {
+//     //   console.log(message);
+//     // }
+//   }
+// })();
 
-(async () => {
-  const chatStream = await cohere.chatStream({
-    chatHistory: [
-    ],
-    message: "Write me a question about marine pollution and include the right answer",
-    model: 'command-light',
-    // documents: articlesData,
-    // promptTruncation: "AUTO"
-    connectors: [{ id: "web-search" }],
-    temperature: 0.6,
-    stream: true
-  });
+// (async () => {
+//   const chatStream = await cohere.chatStream({
+//     chatHistory: [
+//     ],
+//     message: "Write me a question about marine pollution and include the right answer",
+//     model: 'command-light',
+//     // documents: articlesData,
+//     // promptTruncation: "AUTO"
+//     connectors: [{ id: "web-search" }],
+//     documents: [
+//       {
+//         id: '1',
+//         title: 'What are the main causes of marine pollution?',
+//         text: 'Tanker spills, backwater contamination, Incandues waste, Atmospheric pollution, Runoff from land (from agriculture or urban storm drains), Nonpoint source pollution, Natural disasters'
+//       },
+//     ],
+//     temperature: 0.6,
+//     stream: true
+//   });
 
-  for await (const message of chatStream) {
-    if(message.eventType == 'stream-end' && message.is_finished == true)
-      console.log(message.response.text);
-  }
-})();
+//   for await (const message of chatStream) {
+//     // if(message.eventType == 'stream-end' && message.is_finished == true)
+//       // console.log(message);
+//   }
+// })();
